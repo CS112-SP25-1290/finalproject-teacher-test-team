a0c501dc79c7390f699741eafad2f99e38d4321b
David Charles Q <davidcharlesquinto@gmail.com>
2025-05-04 14:27:00 -0700

    Doing this in my gaming pc.

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/HelloController.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/HelloController.java
@@ -1,14 +1,39 @@
 package edu.miracosta.cs112.finalproject.finalproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    Label titleLabel
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void initialize() {
+        titleLabel.setText("World Brain Test");
+        Image image = new Image(REDSUMMER_IMAGE_PATH);
+        imageLabel.setImage(image);
+    @FXML
+    public void handleTakeQuizButton(ActionEvent event) {
+        Stage stage = (Stage)titleLabel.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("full-name-view"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("World Brain Test");
+        stage.show();
+    }
+    @FXML
+    public void handleExitButton() {
+        System.exit(0);
     }
 }
