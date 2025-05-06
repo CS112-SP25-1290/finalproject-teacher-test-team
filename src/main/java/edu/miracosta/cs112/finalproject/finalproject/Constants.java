5ae6487262690f4e57fd3e81ceb9c31583769535
David Charles Q <davidcharlesquinto@gmail.com>
2025-05-05 20:12:25 -0700

    Doing this in my gaming pc.

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/Constants.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/Constants.java
@@ -1,9 +1,14 @@
 package edu.miracosta.cs112.finalproject.finalproject;
 
 public class Constants {
+    public static final String MAIN_PROGRAM_TITLE = "World Brain Test";
     public static final String MAIN_PROGRAM_DESCRIPTION = "World Brain Test";
 
     public static final String INTRO_VIEW_RESOURCE = "intro-view.fxml";
     public static final String FULL_NAME_VIEW_RESOURCE = "full-name-view.fxml";
     public static final String INSTRUCTIONS_VIEW_RESOURCE = "instructions.fxml";
+
+    public static final String[][] quizData {
+
+    }
 }
--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/HelloController.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/HelloController.java
@@ -6,13 +6,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
     @FXML
-    Label titleLabel
+    Label titleLabel;
+    @FXML
+    ImageView imageLabel;
 
     @FXML
     protected void onHelloButtonClick() {
