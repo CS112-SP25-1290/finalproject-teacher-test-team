091307b691fe289b7a45f8352033a9adba8e665d
David Charles Q <davidcharlesquinto@gmail.com>
2025-05-06 20:48:46 -0700

    Doing this in my gaming pc.

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/HelloController.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/HelloController.java
@@ -4,10 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static edu.miracosta.cs112.finalproject.finalproject.Constants.*;
 
 public class HelloController {
     @FXML
@@ -16,6 +21,8 @@ public class HelloController {
     Label titleLabel;
     @FXML
     ImageView imageLabel;
+    @FXML
+    Button takeQuizButton;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -24,19 +31,22 @@ public class HelloController {
 
     public void initialize() {
         titleLabel.setText("World Brain Test");
-        Image image = new Image(REDSUMMER_IMAGE_PATH);
+        Image image = new Image(EARTH_IMAGE_PATH);
         imageLabel.setImage(image);
-    @FXML
-    public void handleTakeQuizButton(ActionEvent event) {
-        Stage stage = (Stage)titleLabel.getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("full-name-view"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.setTitle("World Brain Test");
-        stage.show();
     }
+
     @FXML
     public void handleExitButton() {
         System.exit(0);
+    }
+
+    @FXML
+    public void handleTakeQuizButton() throws IOException {
+        Stage stage = (Stage) titleLabel.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(FULL_NAME_VIEW_RESOURCE));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle(MAIN_PROGRAM_TITLE);
+        stage.show();
     }
 }
